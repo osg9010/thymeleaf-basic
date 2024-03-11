e0062e1b676b6fde8bbee8787705bbea7cb3098a
osg9010 <osg3419010@naver.com>
2024-03-11 22:53:38 +0900

    - 타임리프 EL

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,9 +1,14 @@
 package hello.thymeleaf.basic;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 @Controller
 @RequestMapping("/basic")
@@ -20,5 +25,32 @@ public class BasicController {
         model.addAttribute("data","Hello <b>Spring</b>");
 
         return "basic/text-unescaped";
+    }
+
+    @GetMapping("/variable")
+    public String variable(Model model){
+        User userA = new User("userA", 10);
+        User userB = new User("userB", 20);
+
+        ArrayList<User> list = new ArrayList<>();
+        list.add(userA);
+        list.add(userB);
+        HashMap<String, User> map = new HashMap<String, User>();
+        map.put(userA.getUsername(),userA);
+        map.put(userB.getUsername(),userB);
+
+        model.addAttribute("user",userA);
+        model.addAttribute("users",list);
+        model.addAttribute("userMap",map);
+
+        return "basic/variable";
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class User{
+        private String username;
+        private int age;
+
     }
 }
