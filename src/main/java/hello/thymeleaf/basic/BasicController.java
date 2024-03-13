31b478053932657adb99c1d7aeea2d375a2a2395
osg9010 <osg3419010@naver.com>
2024-03-13 19:17:53 +0900

    - 타임리프 유틸리티

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -60,6 +61,11 @@ public class BasicController {
         return "basic/basic-objects";
     }
 
+    @GetMapping("/date")
+    public String date(Model model){
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "basic/date";
+    }
 
     @Component("helloBean")
     static class HelloBean {
