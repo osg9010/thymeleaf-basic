6cefba5443ab1a8621af2a79cc80b532bfc5c36f
osg9010 <osg3419010@naver.com>
2024-03-20 23:04:53 +0900

    - 타임리프 조건문

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -107,6 +107,12 @@ public class BasicController {
         return "basic/each";
     }
 
+    @GetMapping("/condition")
+    public String condition(Model model){
+        addUsers(model);
+        return "basic/condition";
+    }
+
     private void addUsers(Model model){
         ArrayList<User> list = new ArrayList<User>();
         list.add(new User("userA",10));
