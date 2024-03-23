34a30e3027e67e0bb4376d94e00f1275c67ddbc7
osg9010 <osg3419010@naver.com>
2024-03-23 13:52:02 +0900

    - 타임리프 자바스크립트 인라인
    문자열이면 큰따움표를, 숫자면 그대로, 객체면 JSON으로 치환해 준다.

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -126,6 +126,13 @@ public class BasicController {
         return "basic/block";
     }
 
+    @GetMapping("/javascript")
+    public String javascript(Model model) {
+        model.addAttribute("user", new User("userA", 10));
+        addUsers(model);
+        return "basic/javascript";
+    }
+
     private void addUsers(Model model){
         ArrayList<User> list = new ArrayList<User>();
         list.add(new User("userA",10));
